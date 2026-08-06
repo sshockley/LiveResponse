658e2903df80b9a6f66f89738cebdfa80388304d
Steve Shockley <steve.shockley@shockley.net>
2026-08-06 13:37:48 +0000

    No idea what I was doing here, fixed.

--- Updates/WUPolicyReset.ps1
+++ Updates/WUPolicyReset.ps1
@@ -7,18 +7,6 @@ $AutoUpdatePath = "HKLM:SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU"
 New-Item -Path $WindowsUpdatePath
 New-Item -Path $AutoUpdatePath
 
-Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 1
-
-Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 0
-Set-ItemProperty -Path $AutoUpdatePath -Name AUOptions -Value 2
-Set-ItemProperty -Path $AutoUpdatePath -Name ScheduledInstallDay -Value 0
-Set-ItemProperty -Path $AutoUpdatePath -Name ScheduledInstallTime -Value 3
-
-Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 0
-Set-ItemProperty -Path $AutoUpdatePath -Name AUOptions -Value 3
-Set-ItemProperty -Path $AutoUpdatePath -Name ScheduledInstallDay -Value 0
-Set-ItemProperty -Path $AutoUpdatePath -Name ScheduledInstallTime -Value 3
-
 Set-ItemProperty -Path $AutoUpdatePath -Name NoAutoUpdate -Value 0
 Set-ItemProperty -Path $AutoUpdatePath -Name AUOptions -Value 4
 Set-ItemProperty -Path $AutoUpdatePath -Name ScheduledInstallDay -Value 0
